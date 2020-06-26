ea5281a373c2866c9738b0783cc7795fc4807858
mykhrd <mykhrd@gmail.com>
2020-06-26 17:03:45 +0900

    implete ejs

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.get("/", function (req, res) {
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
 
     let today = new Date();
     let currentDay = today.getDay();
 
     // Get the day of the week, using local time.
     if (currentDay === 6 || currentDay === 0) {
-        res.send("yay it's the weekend!");
+        res.sendFile(__dirname + '/weekend.html');
     } else {
-        res.send("boo!");
+        res.sendFile(__dirname + '/weekday.html');
     }
 });
 
 app.listen(3000, function () {
-    console.log("Server started on port 3000");
+    console.log('Server started on port 3000');
 })
