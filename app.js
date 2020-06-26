7dcad0ee8daec1707d69e4b30ba8f818c6ff3e04
mykhrd <mykhrd@gmail.com>
2020-06-26 17:26:08 +0900

    add dayOfWeek

--- app.js
+++ app.js
@@ -8,14 +8,25 @@ app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
 
     let today = new Date();
-    let currentDay = today.getDay();
+    let dayOfWeek = new Array(7);
+
+    dayOfWeek[0] = "Sunday";
+    dayOfWeek[1] = "Monday";
+    dayOfWeek[2] = "Tuesday";
+    dayOfWeek[3] = "Wednesday";
+    dayOfWeek[4] = "Thursday";
+    dayOfWeek[5] = "Friday";
+    dayOfWeek[6] = "Saturday";
+
+    let currentDay = dayOfWeek[today.getDay()];
+    //let day = "";
 
     // Get the day of the week, using local time.
-    if (currentDay === 6 || currentDay === 0) {
-        res.sendFile(__dirname + '/weekend.html');
-    } else {
-        res.sendFile(__dirname + '/weekday.html');
-    }
+    //day = currentDay;
+
+    res.render("list", {
+        kindOfDay: currentDay
+    });
 });
 
 app.listen(3000, function () {
